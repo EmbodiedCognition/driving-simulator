2957812108ff7ab4a7caa4bf26316a165a1c6780
Leif Johnson <leif@leifjohnson.net>
2012-09-06 11:13:27 -0500

    When generating data for an experiment, do 30 runs of each condition.

--- bin/experiment.sh
+++ bin/experiment.sh
@@ -6,7 +6,7 @@ for t in 0.1 0.2 0.5 1 2 5 10
 do
     for s in 0.05 0.10 0.20
     do
-        for i in $(seq -w 0 10)
+        for i in $(seq -w 1 30)
         do
             echo "$t $s $i"
             python main.py \
