cb356fa9ad4129667116dea5a9519e3cf67702a4
Leif Johnson <leif@leifjohnson.net>
2013-04-09 18:58:37 -0500

    Change parameters of grid search for experimental data.

--- bin/experiment.sh
+++ bin/experiment.sh
@@ -4,21 +4,23 @@ N=${1:-10}
 
 mkdir -p data
 
-for fs in 0.01 0.02 0.05 0.1 0.2 0.5
+for fs in 0.1 0.2 0.5
 do
-    for st in 1 2 5
+    for st in 1 2 3
     do
-        for ss in 0.01 0.02 0.05 0.1 0.2 0.5
+        for ss in 0.5
         do
             for i in $(seq -w $N)
             do
                 echo "follow 1,$fs speed $st,$ss: $i"
-                python main.py \
+                ( nice python main.py \
                     --follow-threshold 1 \
                     --follow-step $fs \
                     --speed-threshold $st \
                     --speed-step $ss \
-                    > data/follow-1-$fs-speed-$st-$ss.$i.log
+                    --lane-threshold 10 \
+                    --lane-step 0.001 \
+                    > data/follow-1-$fs-speed-$st-$ss.$i.log & )
             done
         done
     done
