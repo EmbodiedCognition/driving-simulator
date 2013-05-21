2efe7ef17141f86b0cf42f72f79163eb73283a8a
Leif Johnson <leif@leifjohnson.net>
2013-05-21 07:38:11 -0500

    Replace experiment script fork-bomb with a (more) proper parallel implementation.

--- bin/experiment.sh
+++ bin/experiment.sh
@@ -2,26 +2,43 @@
 
 N=${1:-10}
 
+SCRIPT=$(mktemp)
+
+cat <<EOF > $SCRIPT
+i=\$(echo \$1 | cut -d' ' -f1)
+fs=\$(echo \$1 | cut -d' ' -f2)
+st=\$(echo \$1 | cut -d' ' -f3)
+ss=\$(echo \$1 | cut -d' ' -f4)
+lt=\$(echo \$1 | cut -d' ' -f5)
+ls=\$(echo \$1 | cut -d' ' -f6)
+python main.py \
+  --follow-threshold 1 \
+  --follow-step \$fs \
+  --speed-threshold \$st \
+  --speed-step \$ss \
+  --lane-threshold \$lt \
+  --lane-step \$ls \
+  > data/follow-1-\$fs-speed-\$st-\$ss-lane-\$lt-\$ls.\$i.log
+echo "follow 1,\$fs speed \$st,\$ss lane \$lt,\$ls: \$i"
+EOF
+
+chmod +x $SCRIPT
+
 mkdir -p data
 
-for fs in 0.1 0.2 0.5
-do
-    for st in 1 2 3
-    do
-        for ss in 0.5
-        do
-            for i in $(seq -w $N)
-            do
-                echo "follow 1,$fs speed $st,$ss: $i"
-                ( nice python main.py \
-                    --follow-threshold 1 \
-                    --follow-step $fs \
-                    --speed-threshold $st \
-                    --speed-step $ss \
-                    --lane-threshold 10 \
-                    --lane-step 0.001 \
-                    > data/follow-1-$fs-speed-$st-$ss.$i.log & )
-            done
-        done
-    done
+# use xargs to run as many jobs in parallel as we have cores on this machine.
+for i in $(seq -w $N); do
+for fs in 0.05 0.1 0.2 0.5; do
+for st in 1 2 5; do
+for ss in 0.05 0.1 0.2 0.5; do
+for lt in 2 5 10; do
+for ls in 0.05 0.1 0.2 0.5; do
+echo -ne "$i $fs $st $ss $lt $ls \0 "
 done
+done
+done
+done
+done
+done | xargs -P$(( $(nproc) - 1 )) -L1 -0 $SCRIPT
+
+rm -f $SCRIPT
