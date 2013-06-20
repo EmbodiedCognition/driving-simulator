dabec12de511b60dc85b61a2e861a8ba6b7b4a19
Leif Johnson <leif@leifjohnson.net>
2013-06-20 14:56:40 -0500

    Update aggregation script to plot human data, properly-normalized joint histograms.

--- analysis/aggregate-runs.py
+++ analysis/aggregate-runs.py
@@ -13,6 +13,9 @@ import sys
 
 from matplotlib import pyplot as plt
 
+BINS = 10
+CONDITIONS = ('follow', 'follow+noise', 'speed', 'speed+noise')
+
 
 def kl(p, q):
     '''Compute KL(p || q).'''
@@ -21,11 +24,11 @@ def kl(p, q):
     return (p[valid] * np.log(p[valid] / (q[valid] + 1e-10))).sum()
 
 
-def load(pattern, mods, bins=102):
+def load(pattern, mods):
     '''Aggregate multiple runs of a condition into one numpy array.'''
     last = lambda x: x.strip().split()[-1]
     runs = []
-    bins = np.arange(bins)
+    bins = np.arange(102)
     for f in glob.glob(pattern + '*.log'):
         looks = dict((i, [0]) for i in mods)
         with open(f) as handle:
@@ -43,42 +46,62 @@ def load(pattern, mods, bins=102):
 def main(pattern, modules=3, humans=None):
     mods = list(range(modules))
 
-    # load human data if available
-    if humans:
-        human = np.loadtxt(humans)
+    logging.info(pattern)
+
+    humans = np.load('human-data.npy').mean(axis=0)
+    humans = np.hstack([humans[:, :BINS, 0], humans[:, BINS:, 0].sum(axis=1)[:, None],
+                        humans[:, :BINS, 1], humans[:, BINS:, 1].sum(axis=1)[:, None],
+                        ])
+    logging.info('computed human joint histograms: %s', humans.shape)
 
     # extract and save numpy look duration data.
     looks = load(pattern, mods)
-    logging.info('%s: saving %s histograms', pattern, looks.shape)
+    logging.info('saving %s histograms', looks.shape)
     np.save(pattern + '.npy', looks)
-    a, b, c = looks.shape
-    np.savetxt(pattern + '.npy.txt', looks.reshape((a, b * c)), fmt='%d')
 
-    # normalize bins into probability distributions
-    sums = looks.sum(axis=2)
-    sums[sums == 0] = -1
-    logging.debug('observation counts:\n%s', sums)
+    # save a text-format version of the joint histograms too.
+    joint = looks.mean(axis=0)[:2]
+    joint = np.hstack([joint[0, :BINS], [joint[0, BINS:].sum()],
+                       joint[1, :BINS], [joint[1, BINS:].sum()],
+                       ])
+    np.savetxt(pattern + '-joint.txt', joint, fmt='%d')
+    logging.info('computed model joint histogram: %s', joint.shape)
 
-    pdfs = looks / sums[:, :, None]
-    means = pdfs.mean(axis=0)
-    stds = pdfs.std(axis=0)
+    # normalize joint counts into probability distributions
+    humans /= humans.sum(axis=-1)[:, None]
+    joint /= joint.sum()
+    for i in range(len(humans)):
+        k = kl(joint / joint.sum(), humans[i] / humans[i].sum())
+        logging.info('%15s: KL from model to humans: %.4f', CONDITIONS[i], k)
 
     # compute kl distr between individual runs and group mean; plot histograms
     colors = ('#2ca02c', '#1f77b4', '#9467bd')
     shapes = 'so'
-    t = np.arange(len(means[0])).astype(float)
+    means = looks.mean(axis=0)
+    t = np.arange(BINS + 1).astype(float)
     tt = np.linspace(0, t[-1], t[-1] * 11)
+
+    for c in range(len(CONDITIONS)):
+        ax = plt.subplot(111)
+        for i in (0, 1):
+            a = i * (BINS + 1)
+            b = (i + 1) * (BINS + 1)
+            ax.plot(t * 0.3, humans[c, a:b], '%s-' % shapes[i], c=colors[i], mew=0)
+        ax.set_xlim(0, 3)
+        ax.set_xticks(np.linspace(0, 3, 4))
+        ax.set_ylim(0, 0.5)
+        ax.set_yticks(np.linspace(0, 0.5, 3))
+        plt.gcf().set_size_inches(3, 2)
+        if c == 0:
+            plt.tight_layout()
+        plt.savefig('data/human-%s.pdf' % CONDITIONS[c], dpi=1200)
+        plt.clf()
+
     ax = plt.subplot(111)
-    for i in mods[:2]:
-        if humans is not None and humans.shape[1] > i:
-            logging.info('%s: [%d] KL to human %.3f', kl(means[i], humans[:, i]))
-
-        k = np.array([kl(means[i], m) for m in pdfs[:, i, :]])
-        logging.info('%s: [%d] KL to mean %.3f +/- %.4f', pattern, i, k.mean(), k.std())
-        mu = means[i]
-        sem = stds[i] / np.sqrt(len(pdfs))
-
-        ax.plot(t * 0.3, mu, '%s-' % shapes[i], c=colors[i], mew=0)
+    for i in (0, 1):
+        a = i * (BINS + 1)
+        b = (i + 1) * (BINS + 1)
+        ax.plot(t * 0.3, joint[a:b], '%s-' % shapes[i], c=colors[i], mew=0)
 
         #above = SI.UnivariateSpline(t, mu + sem)
         #below = SI.UnivariateSpline(t, mu - sem)
@@ -88,10 +111,9 @@ def main(pattern, modules=3, humans=None):
 
     ax.set_xlim(0, 3)
     ax.set_xticks(np.linspace(0, 3, 4))
-    ax.set_ylim(0, 1)
-    ax.set_yticks(np.linspace(0, 1, 3))
+    ax.set_ylim(0, 0.5)
+    ax.set_yticks(np.linspace(0, 0.5, 3))
     plt.gcf().set_size_inches(3, 2)
-    plt.tight_layout()
     plt.savefig(pattern + '.pdf', dpi=1200)
 
 
